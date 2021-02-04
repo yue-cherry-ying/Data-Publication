fb883151bf48092618300eaa3af3c1122fd26767
yue-cherry-ying <yue.cherry.ying@outlook.com>
2021-02-04 11:53:55 -0600

    working on chalkboard

--- research_website/js/chalkboard.js
+++ research_website/js/chalkboard.js
@@ -22,9 +22,19 @@ function variable() {
 
 function evalNumber() {
     var inputValue = Number(prompt("Enter any five-digit number without commas"))
-    if (inputValue % 2 == 0) {
+    if (isNaN(inputValue) || Number.isInteger(inputValue)) { // check to see if the input value is not a number or if is not an integer
+        alert(inputValue + " is not a number or is not an integer.")
+    } else if (inputValue.toString().length != 5) {
+        alert("False! " + inputValue + " is not a whole 5-digit number.")
+    } else if (inputValue % 2 == 0) {
         alert(inputValue + " is an even number.")
     } else {
         alert(inputValue + " is an odd number.")
     }
+}
+
+function changeTitle() {
+    let selectedElement = document.getElementById("week1Card");
+    console.log(selectedElement);
+    selectedElement.innerText = "Week One"
 }
