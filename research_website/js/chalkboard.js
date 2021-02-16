3ef7960b9b7d6cf293cb935ad70572d11553faa9
yue-cherry-ying <yue.cherry.ying@outlook.com>
2021-02-15 18:46:22 -0600

    following the video

--- research_website/js/chalkboard.js
+++ research_website/js/chalkboard.js
@@ -70,12 +70,23 @@ function disappear() {
     selectedElement.style.visibility = "hidden";
 }
 
-
 function lastItem(arrayName) {
     let selectedElement = document.getElementById("fruit");
     arrayName.sort();
-    selectedElement.innerText = "The original array is " + arrayName + ", and I sorted it alphabetically. The last item of the sorted array is " + arrayName[arrayName.length - 1];
+    selectedElement.innerText = "The original array is " + "[" +
+        arrayName +
+        "], and I sorted it alphabetically. The last item of the sorted array is " + arrayName[arrayName.length - 1];
 }
+
+// function parseArray(array) {
+//     var newFruit = prompt("enter a fruit"); //prompt asks for input
+//     array.push(newFruit); //.push method adds a value to an array
+//     var x = array.sort(); //.sort method sorts values in an array
+//     var y = x.length; //.length method accesses the length of an array
+//     console.log(x[y - 1]); //log the last item in the array
+//     console.log(array); //log the entire array
+// }
+// var newArray = ["papaya", "apple", "orange", "banana"];
 
 function parentFunction() {
     let a = 1;
@@ -85,4 +96,62 @@ function parentFunction() {
         return b;
     };
     return childFunction();
+}
+
+function searchMusic() {
+    // declare variable from user input
+    var artistName = document.getElementById("artistInput").value;
+    // var albumName = document.getElementById("albumInput").value;
+
+    // declare base url for API
+    var url = "https://www.theaudiodb.com/api/v1/json/1/discography.php?s=" + artistName;
+
+    // declare destination for album art
+    // var albumDiv = document.getElementById("albumArt");
+
+    // fetch command
+    // check for bad response
+    // save the response to a variable
+    // grab album art URL
+    // grab album year
+    // grab album genre
+    // grab album description
+    fetch(url)
+        .then( // check to see if the page is not loading properly
+            function(response) {
+                if (response.status !== 200) {
+                    // 200 means that the page is successfully hit
+                    console.log('PROBLEM! Status code is: ' + response.status);
+                    return;
+                }
+                response.json().then(function(data) {
+                    console.log(data);
+                    let jsonContent = data.album;
+                    console.log(jsonContent); // this allows us to investigate what fields are available to us
+                    for (i in jsonContent) {
+                        var discographyDiv = document.getElementById('discography');
+                        var albumYearDiv = document.createElement('span');
+                        albumYearDiv.setAttribute('class', 'h4');
+                        albumYearDiv.innerText = jsonContent[i].intYearReleased;
+                        var albumNameDiv = document.createElement('span');
+                        albumNameDiv.setAttribute('class', 'h4');
+                        albumNameDiv.innerText = jsonContent[i].strAlbum;
+                        discographyDiv.appendChild(albumYearDiv);
+                        discographyDiv.appendChild(albumNameDiv);
+                    }
+                    // albumDiv.src = jsonContent.strAlbumThumb; // change the src of the albumDiv id
+                    document.getElementById("albumYear").innerText = jsonContent.intYearReleased;
+                    document.getElementById("albumName").innerText = jsonContent.strAlbum;
+                    // document.getElementById("albumDesc").innerText = jsonContent.strDescriptionEN;
+                });
+            });
+}
+
+// Super Challenge
+// Use this base to return all albums.
+// Iterate through them all and display them.
+// https: //www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=" + artistName + "&a=" + albumName;
+
+function addElements() {
+    var valueArray = ['first', 'second', 'third'];
 }
