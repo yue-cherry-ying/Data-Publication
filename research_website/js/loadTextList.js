5c45fcd08db12558f4dfc9e4cb7e0cedd0dd6ec1
yue-cherry-ying <yue.cherry.ying@outlook.com>
2021-02-23 17:41:56 -0600

    finished with rsti video

--- research_website/js/loadTextList.js
+++ research_website/js/loadTextList.js
@@ -24,8 +24,31 @@ function XMLrequest(link) {
 }
 
 function listTexts(sourceXML) {
+    document.getElementById('projectTitle').innerText = sourceXML.getElementsByTagName('metadata')[0].children[1].innerHTML;
+    document.getElementById('setTitle').innerText = sourceXML.getElementsByTagName('set')[0].children[3].children[0].innerHTML;
+    document.getElementById('setDescription').innerText = sourceXML.getElementsByTagName('set')[0].children[4].innerHTML;
+    var licenseText = document.getElementById('license');
+    licenseText.innerText = sourceXML.getElementsByTagName('availability')[0].children[0].innerHTML;
+    licenseText.setAttribute('href', sourceXML.getElementsByTagName('availability')[0].children[0].attributes[0].nodeValue);
+
     // Select, parse, and display the data
     console.log(sourceXML); // raw document response we get from the API
-    //     var textList = sourceXML.getElementsByTagName('text');
-    //     console.log(textList)
+    var textList = sourceXML.getElementsByTagName('text');
+    console.log(textList);
+    for (i = 0; i < textList.length; i++) {
+        // create one row per text
+        var tr = document.createElement('tr');
+        tr.setAttribute('class', 'ochreTableRows');
+        tr.setAttribute('id', 'row_' + i);
+        document.getElementById('ochreTableBody').appendChild(tr);
+        // populate the cells in the row
+        var td = document.createElement('td');
+        td.setAttribute('id', 'td_name_' + i);
+        td.textContent = textList[i].children[0].children[0].innerHTML;
+        document.getElementById('row_' + i).appendChild(td);
+        var td2 = document.createElement('td');
+        td2.setAttribute('id', 'td_desc_' + i);
+        td2.textContent = textList[i].children[3].innerHTML;
+        document.getElementById('row_' + i).appendChild(td2);
+    }
 }
