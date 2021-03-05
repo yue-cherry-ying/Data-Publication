27d84b0e6fb7a486c54fe53f9d7c9963c116cb81
yue-cherry-ying <yue.cherry.ying@outlook.com>
2021-03-05 09:18:20 -0600

    rsti updated with images

--- research_website/js/loadTextList.js
+++ research_website/js/loadTextList.js
@@ -35,56 +35,82 @@ function listTexts(sourceXML) {
     console.log(sourceXML); // raw document response we get from the API
     var textList = sourceXML.getElementsByTagName('text');
     console.log(textList);
-    var linkList = sourceXML.getElementsByTagName('links');
-    console.log(linkList);
+    xmlDOM = sourceXML;
     for (i = 0; i < textList.length; i++) {
         // create one row per text
         var tr = document.createElement('tr');
-        tr.setAttribute('class', 'ochreTableRows');
-        tr.setAttribute('id', 'row_' + i);
+        tr.className = 'ochreTableRows';
+        // set unique attributes per row
+        tr.id = 'row_' + i;
+
+        // tr.setAttribute('class', 'ochreTableRows');
+        // tr.setAttribute('id', 'row_' + i);
         document.getElementById('ochreTableBody').appendChild(tr);
+
         // populate the cells in the row
         var td = document.createElement('td');
-        td.setAttribute('id', 'td_name_' + i);
-        td.textContent = textList[i].children[0].children[0].innerHTML;
-        // var imageDiv = document.createElement('div');
-        // imageDiv.addEventListener('click', showImage(i, linkList));
-        // imageDiv.textContent = textList[i].children[0].innerHTML;
-        // td.appendChild(imageDiv);
+        td.className = 'Name';
+        td.id = 'td_row_' + i;
+
+        // create an anchor link for each text name
+        var button = document.createElement('button');
+        var textUUID = textList[i].attributes[1].nodeValue;
+        button.id = textUUID;
+        // onclick passes the text UUID and the xmlDOM to the next function
+        button.setAttribute('onclick', 'photoBlock(this.id, xmlDOM)');
+        button.setAttribute('target', '_blank');
+        button.className = 'textSelection btn btn-link';
+        var textLabel = textList[i].childNodes[0].childNodes[0].innerHTML;
+        button.textContent = textLabel;
+
+        // td.setAttribute('id', 'td_name_' + i);
+        // td.textContent = textList[i].children[0].children[0].innerHTML;
+
         document.getElementById('row_' + i).appendChild(td);
+        document.getElementById('td_row_' + i).appendChild(button);
         var td2 = document.createElement('td');
-        td2.setAttribute('id', 'td_desc_' + i);
-        td2.textContent = textList[i].children[3].innerHTML;
+        td2.className = 'Description';
+        // td2.setAttribute('id', 'td_desc_' + i);
+
+        var textDescription = textList[i].childNodes[3].innerHTML;
+        td2.textContent = textDescription;
+        // td2.textContent = textList[i].children[3].innerHTML;
         document.getElementById('row_' + i).appendChild(td2);
-
-
-        // var links = textList[i].children[4];
-        // for (i = 0; i < links.length; i++) {
-        //     var td3 = document.createElement('td');
-        //     td3.setAttribute('id', 'td_photo_' + i);
-        //     var a = document.createElement('a');
-        //     var url = "http://ochre.lib.uchicago.edu/ochre?uuid=" + links.children[i].attributes[1].nodeValue + "&preview";
-        //     a.setAttribute('href', XMLrequest(url));
-        //     td3.textContent = links.children[i].innerHTML;
-        //     document.getElementById('row_' + i).appendChild(a);
-        //     document.getElementById('row_' + i).appendChild(td3);
-        // }
     }
 }
 
-function showImage(i, linkList) {
-    var array = [];
-    for (j = 0; j < linkList[i].children.length; j++) {
-        var resource = listList[i].children[j].outerHTML;
-        var parResource = new window.DOMParser().parseFromString(resource, 'text/xml');
-        var uuid = parResource.getElementsByTagName('resource')[0].attributes[1].nodeValue;
-        var url = "http://ochre.lib.uchicago.edu/ochre?uuid=" + uuid + "&preview";
-        array.push(url);
+function photoBlock(textUUID, xmlDOM) {
+    //console.log(xmlDOM);
+    var imageText = xmlDOM.getElementsByTagName('text');
+    document.getElementById('imgBlock').innerHTML = "";
+    //console.log(imageText);
+    var imageUUIDs = [];
+    for (var i = 0; i < imageText.length; i++) {
+        if (imageText[i].attributes[1].nodeValue === textUUID) {
+            //console.log(imageText[i]);
+            var linksList = imageText[i].getElementsByTagName('resource');
+
+            //console.log(linksList);
+            for (var n = 0; n < linksList.length; n++) {
+                var attUUID = linksList[n].attributes.uuid.nodeValue;
+                var imageUUID = attUUID;
+                imageUUIDs.push(imageUUID);
+            };
+        };
     };
-    var display = document.getElementById('image display');
-    for (j = 0; j < array.length; j++) {
+    //console.log(imageUUIDs);
+    for (var i = 0; i < imageUUIDs.length; i++) {
+        var a = document.createElement('a');
+        a.setAttribute('href', 'http://ochre.lib.uchicago.edu/ochre?uuid=' + imageUUIDs[i] + '&image');
+        a.target = "_blank";
+        a.id = 'link_' + i;
+        document.getElementById('imgBlock').appendChild(a);
         var img = document.createElement('img');
-        img.setAttribute('src', array[j]);
-        display.appendChild(img);
+        img.setAttribute('src', 'http://ochre.lib.uchicago.edu/ochre?uuid=' + imageUUIDs[i] + '&preview');
+        img.id = 'image_' + i;
+        document.getElementById('link_' + i).appendChild(img);
+        var cap = document.createElement('figcaption');
+        cap.innerHTML = linksList[i].innerHTML;
+        document.getElementById('link_' + i).appendChild(cap);
     }
 }
